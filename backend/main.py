ea454e21b1963f4baa2ea9eeea7ed78a419f0706
SimoHellgren <hellgrensimo@gmail.com>
2021-02-24 20:57:37 +0200

    moved collections to separate router

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from typing import List
 from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
-from .routers import songs, composers
+from .routers import songs, composers, collections
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -20,27 +20,8 @@ def get_db():
 
 app.include_router(songs.router)
 app.include_router(composers.router)
+app.include_router(collections.router)
 
 @app.get('/')
 def root():
     return 'hey.'
-
-@app.get('/collections', response_model=List[schemas.Collection])
-def read_collection(db: Session = Depends(get_db)):
-    return crud.get_collections(db)
-
-@app.get('/collections/{collection_id}', response_model=schemas.Collection)
-def read_collection(collection_id: int, db: Session = Depends(get_db)):
-    return crud.get_collection(db, collection_id)
-
-@app.get('/collections/{collection_id}/songs', response_model=List[schemas.Song])
-def read_collection_songs(collection_id: int, db: Session = Depends(get_db)):
-    return crud.get_collection_songs(db, collection_id)
-
-@app.put('/collections/{collection_id}/songs/{song_id}')
-def add_song_to_collection(collection_id: int, song_id: int, db: Session = Depends(get_db)):
-    return crud.add_song_to_collection(db, collection_id, song_id)
-
-@app.get('/collections/search/{q}', response_model=List[schemas.Collection])
-def search_songs_by_collection_name(q: str, db: Session = Depends(get_db)):
-    return crud.search_collections_by_name(db, q)
---
+++ backend/routers/collections.py
@@ -0,0 +1,28 @@
+from typing import List
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..dependencies import get_db
+from .. import schemas
+from .. import crud
+
+router = APIRouter()
+
+@router.get('/collections', response_model=List[schemas.Collection])
+def read_collection(db: Session = Depends(get_db)):
+    return crud.get_collections(db)
+
+@router.get('/collections/{collection_id}', response_model=schemas.Collection)
+def read_collection(collection_id: int, db: Session = Depends(get_db)):
+    return crud.get_collection(db, collection_id)
+
+@router.get('/collections/{collection_id}/songs', response_model=List[schemas.Song])
+def read_collection_songs(collection_id: int, db: Session = Depends(get_db)):
+    return crud.get_collection_songs(db, collection_id)
+
+@router.put('/collections/{collection_id}/songs/{song_id}')
+def add_song_to_collection(collection_id: int, song_id: int, db: Session = Depends(get_db)):
+    return crud.add_song_to_collection(db, collection_id, song_id)
+
+@router.get('/collections/search/{q}', response_model=List[schemas.Collection])
+def search_songs_by_collection_name(q: str, db: Session = Depends(get_db)):
+    return crud.search_collections_by_name(db, q)
