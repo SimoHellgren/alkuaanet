236db59c133f870570df20f4114dca4ba9a01d4a
SimoHellgren <hellgrensimo@gmail.com>
2021-02-24 21:00:46 +0200

    cleaned up unused things

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,11 @@
-from fastapi import FastAPI, Depends
-from typing import List
-from sqlalchemy.orm import Session
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import songs, composers, collections
 
 models.Base.metadata.create_all(bind=engine)
 
-find = lambda f, it, default=None: next(filter(f, it), default)
-
 app = FastAPI()
-
-def get_db():
-    db = SessionLocal()
-    try: 
-        yield db
-    finally:
-        db.close()
 
 app.include_router(songs.router)
 app.include_router(composers.router)
@@ -24,4 +13,4 @@ app.include_router(collections.router)
 
 @app.get('/')
 def root():
-    return 'hey.'
+    return 'Hello there!'
