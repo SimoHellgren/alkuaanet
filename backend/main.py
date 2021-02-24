cd1834079fc99f4e00dcc08ebb907f30075e2ffa
SimoHellgren <hellgrensimo@gmail.com>
2021-02-24 20:52:38 +0200

    moved composers to separate router

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from typing import List
 from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
-from .routers import songs
+from .routers import songs, composers
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -19,34 +19,11 @@ def get_db():
         db.close()
 
 app.include_router(songs.router)
+app.include_router(composers.router)
 
 @app.get('/')
 def root():
     return 'hey.'
-
-@app.get('/composers', response_model=List[schemas.Composer])
-def read_composers(db: Session = Depends(get_db)):
-    return crud.get_composers(db)
-
-@app.post('/composers', response_model=schemas.Composer)
-def create_composer(composer: schemas.ComposerCreate, db: Session = Depends(get_db)):
-    return crud.create_composer(db, composer)
-
-@app.get('/composers/{composer_id}', response_model=schemas.Composer)
-def read_composer(composer_id: int, db: Session = Depends(get_db)):
-    return crud.get_composer(db, composer_id)
-
-@app.get('/composers/{composer_id}/songs', response_model=List[schemas.Song])
-def read_composer_songs(composer_id: int, db: Session = Depends(get_db)):
-    return crud.get_songs_by_composer(db, composer_id)
-
-@app.put('/composers/{composer_id}/songs/{song_id}')
-def add_song_to_composer(composer_id: int, song_id: int, db: Session = Depends(get_db)):
-    return crud.add_song_to_composer(db, composer_id, song_id)
-
-@app.get('/composers/search/{lastname}', response_model=List[schemas.Composer])
-def search_composers(lastname: str, db: Session = Depends(get_db)):
-    return crud.search_composers_by_lastname(db, lastname)
 
 @app.get('/collections', response_model=List[schemas.Collection])
 def read_collection(db: Session = Depends(get_db)):
