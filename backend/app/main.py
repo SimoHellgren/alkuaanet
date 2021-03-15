48085b4df8eb4bdb3954b69d23cc3ea919009868
SimoHellgren <hellgrensimo@gmail.com>
2021-03-15 19:58:35 +0200

    allowed CORS for frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import songs, composers, collections
+from .config import allowed_origins
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins
+)
 
 app.include_router(songs.router)
 app.include_router(composers.router)
