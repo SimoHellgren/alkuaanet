e72091466594cd65272bfd0e43ccb9ff2be13473
SimoHellgren <hellgrensimo@gmail.com>
2021-03-18 17:26:43 +0200

    improve readability

--- frontend/src/components/SongList.js
+++ frontend/src/components/SongList.js
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom'
 
 const SongList = ({songs}) => {
     const [filter, setFilter] = useState("")
-   
+
+    const showSongs = songs.filter(
+      s => s.name.toLowerCase().startsWith(filter.toLowerCase())
+    )
+
     return [
       <h2>Songs:</h2>,
       <form>
         <input id='songsearch' value={filter} onChange={({target}) => setFilter(target.value)}/>
       </form>,
-      <div>{songs.filter(s => s.name.toLowerCase().startsWith(filter.toLowerCase())).map(
-        s => <div><Link to={`/songs/${s.id}`}>[{s.id}] {s.name} ({s.tones})</Link></div>)}</div>
+      <div>{showSongs.map(s =><div><Link to={`/songs/${s.id}`}>[{s.id}] {s.name} ({s.tones})</Link></div>)}</div>
     ]
   }
 
