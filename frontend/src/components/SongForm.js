38f10986d3ba087c90fcb5e5e59d3e0139462574
SimoHellgren <hellgrensimo@gmail.com>
2021-03-18 17:58:57 +0200

    useFormField

--- frontend/src/components/SongForm.js
+++ frontend/src/components/SongForm.js
@@ -1,25 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useFormField } from '../hooks'
 
 const SongForm = ({ createSong }) => {
-    const [name, setName] = useState("")
-    const [tones, setTones] = useState("")
+    const name = useFormField('text', '')
+    const tones = useFormField('text', '')
   
     const addSong = (event) => {
       event.preventDefault()
   
       createSong(
-        { name, tones }
+        { 
+          name: name.value, 
+          tones: tones.value 
+        }
       )
+
+      name.reset()
+      tones.reset()
         
-      setName("")
-      setTones("")
     }
     
     return [
       <h2>Create new song</h2>,
       <form onSubmit={addSong}>
-        <div>name: <input id='name' value={name} onChange={({target}) => setName(target.value)}/></div>
-        <div>tones: <input id='tones' value={tones} onChange={({target}) => setTones(target.value)}/></div>
+        <div>name: <input {...name}/></div>
+        <div>tones: <input {...tones}/></div>
         <button type='submit'>Make!</button>
       </form>
     ]
