536d643794ee3e796ad3eb1c4bc27a41e390ec5b
SimoHellgren <hellgrensimo@gmail.com>
2023-10-19 20:30:51 +0300

    first version of db

--- infra/dynamo.tf
+++ infra/dynamo.tf
@@ -1,17 +1,29 @@
 resource "aws_dynamodb_table" "a-test-table" {
   name         = "songs"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
-  range_key    = "name"
+  hash_key     = "pk"
+  range_key    = "sk"
 
   attribute {
-    name = "id"
-    type = "N"
-  }
-
-  attribute {
-    name = "name"
+    name = "pk"
     type = "S"
   }
 
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  attribute {
+    name = "type"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "LookupIndex"
+    hash_key           = "type"
+    range_key          = "sk"
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["name"]
+  }
 }
