3d9857b81a719bd1aaf0edb497371c3e3865dc0f
SimoHellgren <hellgrensimo@gmail.com>
2023-10-26 18:57:28 +0300

    add song search, utility classmethod

--- graph/schema.py
+++ graph/schema.py
@@ -9,15 +9,25 @@ class Song:
     id: str
     name: str
 
+    @classmethod
+    def from_db(cls, record):
+        return cls(id=record["pk"], name=record["name"])
+
 
 def get_songs() -> list[Song]:
     data = crud.get_songs()["Items"]
-    return [Song(id=song["pk"], name=song["name"]) for song in data]
+    return [Song.from_db(song) for song in data]
+
+
+def search_songs(string: str) -> list[Song]:
+    data = crud.search_songs(f"name:{string}")["Items"]
+    return [Song.from_db(song) for song in data]
 
 
 @strawberry.type
 class Query:
     songs: list[Song] = strawberry.field(resolver=get_songs)
+    search: list[Song] = strawberry.field(resolver=search_songs)
 
 
 schema = strawberry.Schema(query=Query)
