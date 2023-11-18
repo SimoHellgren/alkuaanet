43c4946df86cfc8b18ee9ec45a2f5f1ce7458320
SimoHellgren <hellgrensimo@gmail.com>
2023-11-18 19:20:30 +0200

    lambda func & log group for synth

--- infra/synth_resources.tf
+++ infra/synth_resources.tf
@@ -29,3 +29,20 @@ resource "docker_image" "synth-image-local" {
 resource "docker_registry_image" "synth-image-ecr" {
   name = docker_image.synth-image-local.name
 }
+
+
+resource "aws_lambda_function" "alkuaanet-synth-lambda" {
+  function_name = "alkuaanet-synth"
+  role          = aws_iam_role.iam_for_lambda.arn
+  timeout       = 10
+  image_uri     = "${aws_ecr_repository.alkuaani-synth.repository_url}:latest"
+  package_type  = "Image"
+}
+
+resource "aws_cloudwatch_log_group" "synth_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.alkuaanet-synth-lambda.function_name}"
+  retention_in_days = 7
+  lifecycle {
+    prevent_destroy = false
+  }
+}
