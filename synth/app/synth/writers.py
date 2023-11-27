ef86de843a98ef40f3bdda58e3c8dc6716437f42
SimoHellgren <hellgrensimo@gmail.com>
2023-11-27 15:10:49 +0200

    actually move make_opus_blob

--- synth/app/synth/writers.py
+++ synth/app/synth/writers.py
@@ -8,6 +8,16 @@ import wave
 from io import BytesIO
 import struct
 import subprocess
+from .synth import combine, arpeggio
+from .writers import samples_to_opus
+
+
+def make_opus_blob(tones, tone_duration=1000, offset=500, sample_rate=44100):
+    arpeggio_sound, arpeggio_duration = arpeggio(tones, tone_duration, offset)
+
+    samples = combine(arpeggio_sound, arpeggio_duration, sample_rate)
+    opus_bytes = samples_to_opus(samples, sample_rate)
+    return opus_bytes
 
 
 def samples_to_wav_bytes(samples, sample_rate=44100):
