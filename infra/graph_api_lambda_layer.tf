16972c49a7180e222865e1ff85ed86aa6ee69b97
SimoHellgren <hellgrensimo@gmail.com>
2025-01-01 18:46:59 +0200

    add hash to track changes

--- infra/graph_api_lambda_layer.tf
+++ infra/graph_api_lambda_layer.tf
@@ -1,6 +1,7 @@
 resource "aws_lambda_layer_version" "graph_api_lambda_layer" {
-  filename   = "${path.module}/managed-files/graph_lambda_layer.zip"
-  layer_name = "graphql-deps"
+  filename         = "${path.module}/managed-files/graph_lambda_layer.zip"
+  layer_name       = "graphql-deps"
+  source_code_hash = filebase64sha256("${path.module}/managed-files/graph_lambda_layer.zip")
 
   compatible_runtimes = ["python3.11"]
 }
