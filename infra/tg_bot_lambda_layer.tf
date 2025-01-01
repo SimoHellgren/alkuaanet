f618b48262102c7104735fff73ecdc4f6dc91866
SimoHellgren <hellgrensimo@gmail.com>
2025-01-01 18:49:28 +0200

    add hash to track changes

--- infra/tg_bot_lambda_layer.tf
+++ infra/tg_bot_lambda_layer.tf
@@ -1,6 +1,7 @@
 resource "aws_lambda_layer_version" "telegram_bot_lambda_layer" {
-  filename   = "${path.module}/managed-files/telegram_lambda_layer.zip"
-  layer_name = "telegram-bot-deps"
+  filename         = "${path.module}/managed-files/telegram_lambda_layer.zip"
+  layer_name       = "telegram-bot-deps"
+  source_code_hash = filebase64sha256("${path.module}/managed-files/telegram_lambda_layer.zip")
 
   compatible_runtimes = ["python3.11"]
 }
