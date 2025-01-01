e1ce2ba82cf57d6a5c2135faebb1c1a5b92078ba
SimoHellgren <hellgrensimo@gmail.com>
2025-01-01 19:31:22 +0200

    don't destroy old layers

--- infra/graph_api_lambda_layer.tf
+++ infra/graph_api_lambda_layer.tf
@@ -2,6 +2,7 @@ resource "aws_lambda_layer_version" "graph_api_lambda_layer" {
   filename         = "${path.module}/managed-files/graph_lambda_layer.zip"
   layer_name       = "graphql-deps"
   source_code_hash = filebase64sha256("${path.module}/managed-files/graph_lambda_layer.zip")
+  skip_destroy     = true
 
   compatible_runtimes = ["python3.11"]
 }
--- infra/tg_bot_lambda_layer.tf
+++ infra/tg_bot_lambda_layer.tf
@@ -2,6 +2,7 @@ resource "aws_lambda_layer_version" "telegram_bot_lambda_layer" {
   filename         = "${path.module}/managed-files/telegram_lambda_layer.zip"
   layer_name       = "telegram-bot-deps"
   source_code_hash = filebase64sha256("${path.module}/managed-files/telegram_lambda_layer.zip")
+  skip_destroy     = true
 
   compatible_runtimes = ["python3.11"]
 }
