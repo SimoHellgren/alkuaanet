25d4c4e3c5adaba968d52caadd200a1705d20c05
SimoHellgren <hellgrensimo@gmail.com>
2025-01-03 10:04:52 +0200

    new table

--- infra/dynamo.tf
+++ infra/dynamo.tf
@@ -27,3 +27,50 @@ resource "aws_dynamodb_table" "songs-table" {
     non_key_attributes = ["name"]
   }
 }
+
+resource "aws_dynamodb_table" "songs-table-v2" {
+  name         = "songs_v2"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "pk"
+  range_key    = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  attribute {
+    name = "search_name"
+    type = "S"
+  }
+
+  attribute {
+    name = "random"
+    type = "N"
+  }
+
+  local_secondary_index {
+    name               = "search_index"
+    range_key          = "search_name"
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["name"]
+  }
+
+  local_secondary_index {
+    name            = "random_index"
+    range_key       = "random"
+    projection_type = "KEYS_ONLY"
+  }
+
+  global_secondary_index {
+    name            = "reverse-index"
+    hash_key        = "sk"
+    range_key       = "pk"
+    projection_type = "KEYS_ONLY"
+  }
+}
