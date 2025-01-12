abcad3a69226a2c7741e2a90b2f79fb9fc1baceb
SimoHellgren <hellgrensimo@gmail.com>
2025-01-12 16:16:38 +0200

    opus creation

--- graph/lib/crud2/opus.py
+++ graph/lib/crud2/opus.py
@@ -1,4 +1,8 @@
+import json
+import boto3
 from lib.models import Opus
+
+LAMBDA = boto3.client("lambda")
 
 
 class CRUDOpus:
@@ -16,6 +20,27 @@ class CRUDOpus:
             return Opus(**item)
 
         return None
+
+    def create(self, table, tones: str):
+        tones_list = tones.split("-")
+
+        # no idea why the payload needs double json.dumps,
+        # should probably figure that out
+        response = LAMBDA.invoke(
+            FunctionName="alkuaanet-synth",
+            InvocationType="RequestResponse",
+            Payload=json.dumps({"body": json.dumps({"tones": tones_list})}),
+        )
+
+        opus = json.loads(response["Payload"].read()).get("body")
+
+        table.put_item(
+            Item={
+                "pk": "opus",
+                "sk": tones,
+                "opus": opus.encode("utf-8"),
+            }
+        )
 
 
 opus = CRUDOpus()
