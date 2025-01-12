ab97be653b36553eba604fbbda2813ec3175ee6f
SimoHellgren <hellgrensimo@gmail.com>
2025-01-12 17:30:40 +0200

    finetune schema

--- graph/lib/schema2.py
+++ graph/lib/schema2.py
@@ -19,16 +19,36 @@ def resolve_opus(tones: str):
     return obj.opus.value.decode("utf-8")
 
 
-@strawberry.experimental.pydantic.type(model=models.Song, all_fields=True)
+def sk_to_id(sk: str) -> int:
+    return int(sk.split(":")[1])
+
+
+@strawberry.type
 class Song:
+    id: int
+    name: str
+    tones: str
 
     @strawberry.field
     def opus(self) -> str:
         return resolve_opus(self.tones)
 
 
-@strawberry.experimental.pydantic.type(model=models.Composer, all_fields=True)
+def get_song(id: int) -> Song:
+    song = crud.songs.get(TABLE, id)
+
+    return Song(
+        id=sk_to_id(song.sk),
+        name=song.name,
+        tones=song.tones,
+    )
+
+
+@strawberry.type
 class Composer:
+    id: int
+    first_name: str
+    last_name: str
 
     # this is already defined in the pydantic model, but for some reason it's not picked up
     @strawberry.field
@@ -36,43 +56,56 @@ class Composer:
         return f"{self.last_name}, {self.first_name}"
 
     @strawberry.field
-    def songs(self) -> list[SearchResult]:
-        _, num = self.sk.split(":")  # ugly
-        return crud.composers.list_songs(TABLE, int(num))
+    def songs(self) -> list[Song]:
+        records = crud.composers.list_songs(TABLE, self.id)
+        return [get_song(sk_to_id(record.sk)) for record in records]
 
 
-@strawberry.experimental.pydantic.type(model=models.Collection, all_fields=True)
+def get_composer(id: int) -> Composer:
+    composer = crud.composers.get(TABLE, id)
+
+    return Composer(
+        id=sk_to_id(composer.sk),
+        first_name=composer.first_name,
+        last_name=composer.last_name,
+    )
+
+
+@strawberry.type
 class Collection:
+    id: int
+    name: str
+
     @strawberry.field
-    def songs(self) -> list[SearchResult]:
-        _, num = self.sk.split(":")  # ugly
-        return crud.collections.list_songs(TABLE, int(num))
+    def songs(self) -> list[Song]:
+        records = crud.collections.list_songs(TABLE, self.id)
+        return [get_song(sk_to_id(record.sk)) for record in records]
+
+
+def get_collection(id: int) -> Collection:
+    collection = crud.collections.get(TABLE, id)
+
+    return Collection(
+        id=sk_to_id(collection.sk),
+        name=collection.name,
+    )
 
 
 @strawberry.type
 class Query:
 
     opus: str = strawberry.field(resolver=resolve_opus)
-
-    @strawberry.field
-    def song(self, id: int) -> Song:
-        return crud.songs.get(TABLE, id)
+    song: Song = strawberry.field(resolver=get_song)
+    composer: Composer = strawberry.field(resolver=get_composer)
+    collection: Collection = strawberry.field(resolver=get_collection)
 
     @strawberry.field
     def search_song(self, string: str) -> list[SearchResult]:
         return crud.songs.search(TABLE, string)
 
     @strawberry.field
-    def composer(self, id: int) -> Composer:
-        return crud.composers.get(TABLE, id)
-
-    @strawberry.field
     def search_composer(self, string: str) -> list[SearchResult]:
         return crud.composers.search(TABLE, string)
-
-    @strawberry.field
-    def collection(self, id: int) -> Collection:
-        return crud.collections.get(TABLE, id)
 
     @strawberry.field
     def search_collection(self, string: str) -> list[SearchResult]:
