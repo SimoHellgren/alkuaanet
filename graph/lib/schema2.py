a1700af9e9fb231947d25c1c615d6dfd560465da
SimoHellgren <hellgrensimo@gmail.com>
2025-01-19 15:43:11 +0200

    replace old with new

--- graph/lib/schema2.py
+++ graph/lib/schema2.py
@@ -2,13 +2,9 @@ from decimal import Decimal
 import strawberry
 from strawberry.asgi import GraphQL
 from mangum import Mangum
-import boto3
 from lib import crud2 as crud
-from lib import models
 from lib import dynamodb as db
 from enum import StrEnum, auto
-
-TABLE = boto3.resource("dynamodb").Table("songs_v2")
 
 
 @strawberry.enum
@@ -40,7 +36,7 @@ class Record:
 
 
 @strawberry.type
-class NewSong(Record):
+class Song(Record):
     # this being optional is only needed to facilitate searching.
     # should add `tones` to the index projection in dynamo instead
     tones: str | None = None
@@ -53,14 +49,14 @@ class NewSong(Record):
 @strawberry.interface
 class Group:
     @strawberry.field
-    def songs(self) -> list[NewSong]:
+    def songs(self) -> list[Song]:
         songs = db.memberships(self.sk)
 
-        return [NewSong(**song) for song in songs]
+        return [Song(**song) for song in songs]
 
 
 @strawberry.type
-class NewComposer(Record, Group):
+class Composer(Record, Group):
     first_name: str | None = None
     last_name: str
 
@@ -72,7 +68,7 @@ class NewComposer(Record, Group):
 
 
 @strawberry.type
-class NewCollection(Record, Group):
+class Collection(Record, Group):
     pass
 
 
@@ -81,122 +77,21 @@ def get_one_of_kind(kind: db.Kind, id: int):
     return db.get_item(kind, sk)
 
 
-def get_new_song(id: int) -> NewSong:
-    return NewSong(**get_one_of_kind(db.Kind.song, id))
-
-
-def get_new_composer(id: int) -> NewComposer:
-    return NewComposer(**get_one_of_kind(db.Kind.composer, id))
-
-
-def get_new_collection(id: int) -> NewCollection:
-    return NewCollection(**get_one_of_kind(db.Kind.collection, id))
-
-
-@strawberry.interface
-class Searchable:
-    """This name is perhaps a touch generic"""
-
-    id: int
-    name: str
-
-
-def resolve_opus(tones: str):
-    obj = crud.opus.get(TABLE, tones)
-    return obj.opus.value.decode("utf-8")
-
-
-def sk_to_id(sk: str) -> int:
-    return int(sk.split(":")[1])
-
-
-@strawberry.type
-class Song(Searchable):
-    tones: str
-
-    @strawberry.field
-    def opus(self) -> str:
-        return resolve_opus(self.tones)
-
-    @classmethod
-    def from_db(cls, record: models.Song):
-        return cls(
-            id=sk_to_id(record.sk),
-            name=record.name,
-            tones=record.tones,
-        )
-
-    @classmethod
-    def from_searchresult(cls, record: models.SearchResult):
-        return cls(
-            id=sk_to_id(record.sk),
-            name=record.name,
-            tones=record.tones,
-        )
-
-
 def get_song(id: int) -> Song:
-    song = crud.songs.get(TABLE, id)
-
-    return Song.from_db(song)
-
-
-@strawberry.type
-class Composer(Searchable):
-    first_name: str | None
-    last_name: str
-
-    # this is already defined in the pydantic model, but for some reason it's not picked up
-    @strawberry.field
-    def name(self) -> str:
-        return f"{self.last_name}, {self.first_name}"
-
-    @strawberry.field
-    def songs(self) -> list[Song]:
-        records = crud.composers.list_songs(TABLE, self.id)
-        return [Song.from_db(record) for record in records]
-
-    @classmethod
-    def from_searchresult(cls, record: models.SearchResult):
-        # this accomodates for empty first name, most notably "trad"
-        last, _, first = record.name.partition(", ")
-
-        return cls(id=sk_to_id(record.sk), first_name=first or None, last_name=last)
+    return Song(**get_one_of_kind(db.Kind.song, id))
 
 
 def get_composer(id: int) -> Composer:
-    composer = crud.composers.get(TABLE, id)
-
-    return Composer(
-        id=sk_to_id(composer.sk),
-        first_name=composer.first_name,
-        last_name=composer.last_name,
-    )
-
-
-@strawberry.type
-class Collection(Searchable):
-
-    @strawberry.field
-    def songs(self) -> list[Song]:
-        records = crud.collections.list_songs(TABLE, self.id)
-        return [Song.from_db(record) for record in records]
-
-    @classmethod
-    def from_searchresult(cls, record: models.SearchResult):
-        return cls(
-            id=sk_to_id(record.sk),
-            name=record.name,
-        )
+    return Composer(**get_one_of_kind(db.Kind.composer, id))
 
 
 def get_collection(id: int) -> Collection:
-    collection = crud.collections.get(TABLE, id)
+    return Collection(**get_one_of_kind(db.Kind.collection, id))
 
-    return Collection(
-        id=sk_to_id(collection.sk),
-        name=collection.name,
-    )
+
+def resolve_opus(tones: str):
+    data = db.get_item("opus", tones)
+    return data["opus"].value.decode("utf-8")
 
 
 @strawberry.type
@@ -207,18 +102,14 @@ class Query:
     composer: Composer = strawberry.field(resolver=get_composer)
     collection: Collection = strawberry.field(resolver=get_collection)
 
-    newsong: NewSong = strawberry.field(resolver=get_new_song)
-    newcomp: NewComposer = strawberry.field(resolver=get_new_composer)
-    newcoll: NewCollection = strawberry.field(resolver=get_new_collection)
-
     @strawberry.field
-    def new_random(self) -> NewSong:
+    def random_song(self) -> Song:
         item = db.random(db.Kind.song)
 
-        return NewSong(**item)
+        return Song(**item)
 
     @strawberry.field
-    def new_search(self, kind: Kind, string: str) -> list[Record]:
+    def search(self, kind: Kind, string: str) -> list[Record]:
         if not string:
             # `kind` is technically of the wrong type for db.list_kind
             records = sorted(db.list_kind(kind), key=lambda x: x["name"])
@@ -228,42 +119,17 @@ class Query:
 
             # a bit janky to do this here, but oh well
             if kind == Kind.composer:
-                return [NewComposer.from_searchresult(record) for record in records]
+                return [Composer.from_searchresult(record) for record in records]
 
         mapping = {
-            Kind.song: NewSong,
-            Kind.composer: NewComposer,
-            Kind.collection: NewCollection,
+            Kind.song: Song,
+            Kind.composer: Composer,
+            Kind.collection: Collection,
         }
 
         model = mapping[kind]
 
         return [model(**record) for record in records]
-
-    @strawberry.field
-    def search(self, kind: Kind, string: str) -> list[Searchable]:
-        mapping = {
-            Kind.song: (crud.songs, Song),
-            Kind.collection: (crud.collections, Collection),
-            Kind.composer: (crud.composers, Composer),
-        }
-
-        klass, model = mapping[kind]
-
-        # this part allows searching with an empty string. Technically the problem
-        # is in crud and not the API, but this is a pretty convenient fix for now
-        if not string:
-            records = sorted(klass.list(TABLE), key=lambda x: x.name)
-        else:
-            records = klass.search(TABLE, string)
-
-        return [model.from_searchresult(record) for record in records]
-
-    @strawberry.field
-    def random_song(self) -> Song:
-        song = crud.songs.random(TABLE)
-
-        return Song.from_db(song)
 
 
 schema = strawberry.Schema(query=Query)
