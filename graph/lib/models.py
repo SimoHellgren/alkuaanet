11f72db9981a5a8ebd185440f7e6593dbe57daf5
SimoHellgren <hellgrensimo@gmail.com>
2025-01-19 16:06:32 +0200

    remove unneeded models

--- graph/lib/models.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-from pydantic import BaseModel
-
-
-class Opus(BaseModel):
-    pk: str
-    sk: str
-    opus: Any  # this should probably be handled better, see: https://docs.pydantic.dev/latest/concepts/types/#handling-third-party-types
