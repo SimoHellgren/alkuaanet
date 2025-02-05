6517ed143f13bc31ede66717e85be6d25f72217c
SimoHellgren <hellgrensimo@gmail.com>
2025-02-05 19:47:59 +0200

    first pass of song command

--- telegram/new_bot.py
+++ telegram/new_bot.py
@@ -2,6 +2,8 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 import os
+import lib.service as api
+
 
 load_dotenv()
 TOKEN = os.environ["DEV_BOT_TOKEN"]
@@ -26,11 +28,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(msg)
 
 
+async def song(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    q = context.args[0]
+    results = api.search("song", q)
+
+    for song in results:
+        await update.message.reply_text(song)
+
+
 app = ApplicationBuilder().token(TOKEN).build()
 
 COMMANDS = [
     hello,
     start,
+    song,
 ]
 
 for command in COMMANDS:
