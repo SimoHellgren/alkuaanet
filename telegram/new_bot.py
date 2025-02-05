1b8827feb1c693fac53894c3d1732dd99cd51fb1
SimoHellgren <hellgrensimo@gmail.com>
2025-02-05 19:39:10 +0200

    start command

--- telegram/new_bot.py
+++ telegram/new_bot.py
@@ -11,9 +11,30 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f"Hello {update.effective_user.name}")
 
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg = (
+        "Tervetuloa alkuäänibottiin!\n\n"
+        "Botti hakee biisejä, säveltäjiä ja kokoelmia. Testaa esimerkiksi:\n\n"
+        "hän\n"
+        "/collections\n"
+        "/collections p\n"
+        "/composers\n"
+        "/composers kuu\n"
+        "/random"
+    )
+
+    await update.message.reply_text(msg)
+
+
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("hello", hello))
+COMMANDS = [
+    hello,
+    start,
+]
+
+for command in COMMANDS:
+    app.add_handler(CommandHandler(command.__name__, command))
 
 
 app.run_polling()
