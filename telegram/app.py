39f4e23a0876865d52e309dc05c626bd6a4665cd
SimoHellgren <hellgrensimo@gmail.com>
2025-02-12 20:49:44 +0200

    restructure

--- telegram/app.py
+++ telegram/app.py
@@ -67,18 +67,9 @@ def handle_callback(bot: Bot, callback_query: CallbackQuery) -> None:
             bot.answer_callback_query(chat_id, callback_query.id)
 
 
-if __name__ == "__main__":
-    import os
-    from time import sleep
-    from dotenv import load_dotenv
-
-    load_dotenv(override=True)
-
-    TOKEN = os.environ.get("DEV_BOT_TOKEN")
-
-    bot = Bot(TOKEN)
-    app = App(
-        bot,
+def build_app(token: str):
+    return App(
+        Bot(token),
         handle_message,
         {
             "/start": start,
@@ -88,21 +79,3 @@ if __name__ == "__main__":
         },
         handle_callback,
     )
-
-    max_update = -1
-    print("Running...")
-    while True:
-        updates = (
-            app.bot.get_updates(params={"offset": max_update + 1})
-            .json()
-            .get("result", [])
-        )
-
-        for update in updates:
-            print("processing", update)
-            app.process_update(update)
-
-        if updates:
-            max_update = max(u["update_id"] for u in updates)
-
-        sleep(1)
